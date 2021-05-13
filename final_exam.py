7cc72087fc53b78e18b7b2aba339bf092e1b0323
Debora <deboribo@gmail.com>
2021-05-13 16:14:03 -0700

    finish exam

--- final_exam.py
+++ final_exam.py
@@ -20,3 +20,17 @@ average = sum of grades / number of grade entries
 sum % of grades > than average
 print results
 """
+
+infile = open("Final.txt", 'r')
+grades = [line.rstrip() for line in infile]
+infile.close()
+for i in range(len(grades)):
+    grades[i] = int(grades[i])
+average = sum(grades) / len(grades)
+num = 0
+for grade in grades:
+    if grade > average:
+        num +=1
+print("Number of grades: ", len(grades))
+print("Average grade: ", average)
+print("Percentage of grades above average: {0:.2f}%".format(100 * num / len(grades)))
