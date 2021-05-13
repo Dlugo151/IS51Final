df55d361748ae6e5653d55dfbf00f343157a3d1e
Debora <deboribo@gmail.com>
2021-05-13 15:59:00 -0700

    finish structured english

---
+++ final_exam.py
@@ -0,0 +1,11 @@
+
+
+
+"""
+we first open file 'Final.txt'
+then we count number of grade entries in the file,
+then we find the average by adding up all the grades and dividing them by the total number of grades entries,
+then find the percentage of grades that are above the average grade
+we close file 'Final.txt'
+finally we output the results
+"""
