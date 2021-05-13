5b3fbf1bf0a59a2ada0ed304d3a53403ead0ce9e
Debora <deboribo@gmail.com>
2021-05-13 16:05:35 -0700

    finish pseudocode

--- final_exam.py
+++ final_exam.py
@@ -4,8 +4,19 @@
 """
 we first open file 'Final.txt'
 then we count number of grade entries in the file,
+we close file 'Final.txt'
 then we find the average by adding up all the grades and dividing them by the total number of grades entries,
 then find the percentage of grades that are above the average grade
-we close file 'Final.txt'
 finally we output the results
+"""
+
+
+"""
+infile = open file 'Final.txt' in read mode
+grades = lines in infile
+close file 'Final.txt'
+count number of grade entries
+average = sum of grades / number of grade entries
+sum % of grades > than average
+print results
 """
